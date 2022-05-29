53f7bea6d022fba3cda6ad64c6eb8a85f0585da4
Dipesh Dulal <nilkantha.dipesh@gmail.com>
2022-05-29 20:19:57 +0545

    feat: just adding a new class

--- hello-world/HelloWorld.java
+++ hello-world/HelloWorld.java
@@ -1,6 +1,28 @@
 class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello world");
-        System.out.println("I am running this from terminal");
+        System.out.println("I am running this from terminal \n");
+
+        World w = new World("Earth");
+        w.sayName();
+    }
+
+}
+
+class World {
+    static String universeName = "my-universe";
+    String name;
+
+    World(String name){
+        this.name = name;
+    }
+
+    public static void main(String[] args) {
+        System.out.println("From world main method");
+    }
+
+    void sayName(){
+        System.out.println("I am from universe: "+ universeName);
+        System.out.println("My planet is: "+ name);
     }
 }

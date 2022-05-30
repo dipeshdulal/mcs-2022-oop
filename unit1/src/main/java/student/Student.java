86b4cdfd478ee97ef3563ae105ff830a1fda3755
Dipesh Dulal <nilkantha.dipesh@gmail.com>
2022-05-30 20:36:23 +0545

    feat: dob class added

--- unit1/src/main/java/student/Student.java
+++ unit1/src/main/java/student/Student.java
@@ -4,6 +4,7 @@ public class Student {
     String firstName;
     String lastName;
     Integer rollNo = 10;
+    DOB dob = new DOB(1, 2022, 12);
 
     Student(String first, String second) {
         firstName = first;
@@ -20,6 +21,22 @@ public class Student {
         System.out.println("Hello! " + firstName + " " + lastName);
         System.out.println("Roll No: " + rollNo.toString());
         System.out.println("HashCode: " + this.hashCode());
+        System.out.println("DOB: " + dob.day + "-" + dob.month + "-" + dob.year);
         System.out.println("--------------");
+    }
+}
+
+class DOB {
+    int day;
+    int year;
+    int month;
+
+    DOB() {
+    }
+
+    DOB(int day, int year, int month) {
+        this.day = day;
+        this.year = year;
+        this.month = month;
     }
 }
